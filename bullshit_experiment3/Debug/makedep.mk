7992a7d08cb12f46dea43c2bbdabc769b7462632
tim <tmktcq@umsystem.edu>
2023-10-20 11:02:41 -0500

    pleasework

---
+++ bullshit_experiment3/Debug/makedep.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+gpio_input.c
+
+gpio_output.c
+
+Long_Serial_In.c
+
+main.c
+
+print_memory.c
+
+SD.c
+
+SPI.c
+
+UART.c
+
+UART_Print.c
+
